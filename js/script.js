8b33ada9d31bd9179c5b5f81d44e62609684f2c1
noah howard <leanneh616@gmail.com>
2026-05-25 13:07:39 -0400

    Finalizing project.

--- js/script.js
+++ js/script.js
@@ -36,19 +36,49 @@ $(document).ready(function () {
   // SPLIT DECK
   /////////////////////////////////////////////////////
 
-  let firstPlayer = deck.slice(0, 26);
+  let player = deck.slice(0, 26);
 
-  let secondPlayer = deck.slice(26);
+  let computer = deck.slice(26);
 
   let playedCards = [];
+
+  /////////////////////////////////////////////////////
+  // COMPARE VARIABLES
+  /////////////////////////////////////////////////////
+
+  let compare = 0;
+
+  let chosen = 0;
+
+  let winner = 0;
+
+  let integerChoice = 0;
+
+  /////////////////////////////////////////////////////
+  // ARITHMETIC VARIABLES
+  /////////////////////////////////////////////////////
+
+  let addition = 0;
+
+  let subtraction = 0;
+
+  let multiplication = 0;
+
+  /////////////////////////////////////////////////////
+  // CURRENT NUMBERS
+  /////////////////////////////////////////////////////
+
+  let currentNumber1 = 0;
+
+  let currentNumber2 = 0;
 
   /////////////////////////////////////////////////////
   // START COUNTS
   /////////////////////////////////////////////////////
 
-  $('#player1Count').html(firstPlayer.length);
+  $('#player1Count').html(player.length);
 
-  $('#player2Count').html(secondPlayer.length);
+  $('#player2Count').html(computer.length);
 
   /////////////////////////////////////////////////////
   // SUIT DISPLAY
@@ -71,13 +101,39 @@ $(document).ready(function () {
 
   function getCardDisplay(number) {
 
-    if (number < 11) return number;
+    let absoluteValue = Math.abs(number);
 
-    if (number === 11) return "<img src='./images/jack.png'>";
+    if (absoluteValue < 11) {
 
-    if (number === 12) return "<img src='./images/queen.png'>";
+      return number;
+    }
 
-    if (number === 13) return "<img src='./images/king.png'>";
+    if (absoluteValue === 11) {
+
+      if (number < 0) {
+        return "-<img src='./images/jack.png'>";
+      }
+
+      return "<img src='./images/jack.png'>";
+    }
+
+    if (absoluteValue === 12) {
+
+      if (number < 0) {
+        return "-<img src='./images/queen.png'>";
+      }
+
+      return "<img src='./images/queen.png'>";
+    }
+
+    if (absoluteValue === 13) {
+
+      if (number < 0) {
+        return "-<img src='./images/king.png'>";
+      }
+
+      return "<img src='./images/king.png'>";
+    }
   }
 
   /////////////////////////////////////////////////////
@@ -101,9 +157,26 @@ $(document).ready(function () {
 
   function updateCounts() {
 
-    $('#player1Count').html(firstPlayer.length);
+    $('#player1Count').html(player.length);
 
-    $('#player2Count').html(secondPlayer.length);
+    $('#player2Count').html(computer.length);
+  }
+
+  /////////////////////////////////////////////////////
+  // HIDE DIRECTIONS (UPDATED FOR MOBILE + DESKTOP)
+  /////////////////////////////////////////////////////
+
+  function hideDirections() {
+
+    $('.compareDirections').addClass('hidden');
+
+    $('.integerDirections').addClass('hidden');
+
+    $('.addDirections').addClass('hidden');
+
+    $('.subtractDirections').addClass('hidden');
+
+    $('.multiplyDirections').addClass('hidden');
   }
 
   /////////////////////////////////////////////////////
@@ -112,22 +185,149 @@ $(document).ready(function () {
 
   function endGame() {
 
-    if (firstPlayer.length === 0) {
+    if (player.length === 0) {
 
-      $('#winner').html("🏆 SECOND PLAYER WINS THE GAME!");
-
+      $('#winner').html("🏆 COMPUTER WINS THE GAME!");
     }
 
-    else if (secondPlayer.length === 0) {
+    else if (computer.length === 0) {
 
-      $('#winner').html("🏆 FIRST PLAYER WINS THE GAME!");
+      $('#winner').html("🏆 PLAYER WINS THE GAME!");
     }
-
-    // DISABLE BUTTON
 
     $('#draw').prop('disabled', true);
 
     $('#draw').css('opacity', '.5');
+  }
+
+  /////////////////////////////////////////////////////
+  // COMPARE MATH
+  /////////////////////////////////////////////////////
+
+  function compareMath(number1, number2) {
+
+    if (number1 > number2) {
+
+      winner = 1;
+    }
+
+    else {
+
+      winner = 2;
+    }
+  }
+
+  /////////////////////////////////////////////////////
+  // ARITHMETIC CHECK
+  /////////////////////////////////////////////////////
+
+  function arithmeticCheck() {
+
+    let answer = $('#answer').val().trim();
+
+    answer = Number(answer);
+
+    let correctAnswer;
+
+    if (addition == 1) {
+
+      correctAnswer =
+        currentNumber1 + currentNumber2;
+    }
+
+    else if (subtraction == 1) {
+
+      correctAnswer =
+        currentNumber1 - currentNumber2;
+    }
+
+    else if (multiplication == 1) {
+
+      correctAnswer =
+        currentNumber1 * currentNumber2;
+    }
+
+    if (answer === correctAnswer) {
+
+      chosen = 1;
+    }
+
+    else {
+
+      chosen = 2;
+    }
+
+    mathCheck(correctAnswer);
+
+    $('#answer').val('');
+  }
+
+  /////////////////////////////////////////////////////
+  // MATH CHECK
+  /////////////////////////////////////////////////////
+
+  function mathCheck(correctAnswer) {
+
+    if (chosen === 1) {
+
+      player.push(...playedCards);
+
+      $('#winner').html("✅ Correct! Player Wins Round");
+
+      $('#firstPlayer').css('border-color', 'red');
+    }
+
+    else {
+
+      computer.push(...playedCards);
+
+      $('#winner').html("❌ Wrong! Correct Answer Was " + correctAnswer);
+
+      $('#secondPlayer').css('border-color', 'red');
+    }
+
+    playedCards = [];
+
+    updateCounts();
+
+    if (player.length === 0 || computer.length === 0) {
+
+      endGame();
+    }
+  }
+
+  /////////////////////////////////////////////////////
+  // COMPARE CHECK
+  /////////////////////////////////////////////////////
+
+  function compareCheck() {
+
+    if (chosen === winner) {
+
+      player.push(...playedCards);
+
+      $('#winner').html("Correct! Player Wins Round");
+
+      $('#firstPlayer').css('border-color', 'red');
+    }
+
+    else {
+
+      computer.push(...playedCards);
+
+      $('#winner').html("Wrong! Computer Wins Round");
+
+      $('#secondPlayer').css('border-color', 'red');
+    }
+
+    playedCards = [];
+
+    updateCounts();
+
+    if (player.length === 0 || computer.length === 0) {
+
+      endGame();
+    }
   }
 
   /////////////////////////////////////////////////////
@@ -136,15 +336,11 @@ $(document).ready(function () {
 
   function drawCard() {
 
-    // RESET BORDERS
-
     $('#firstPlayer').css('border-color', 'black');
 
     $('#secondPlayer').css('border-color', 'black');
 
-    // CHECK GAME OVER
-
-    if (firstPlayer.length === 0 || secondPlayer.length === 0) {
+    if (player.length === 0 || computer.length === 0) {
 
       endGame();
 
@@ -155,9 +351,9 @@ $(document).ready(function () {
 
     $('#secondPlayerSuit').empty();
 
-    let card1 = firstPlayer.shift();
+    let card1 = player.shift();
 
-    let card2 = secondPlayer.shift();
+    let card2 = computer.shift();
 
     let number1 = card1[0];
 
@@ -168,79 +364,101 @@ $(document).ready(function () {
     let suit2 = card2[1];
 
     /////////////////////////////////////////////////////
-    // SHOW CARDS
+    // INTEGER MODE
     /////////////////////////////////////////////////////
+
+    if (integerChoice == 1) {
+
+      if (suit1 == 'hearts' || suit1 == 'diamonds') {
+
+        number1 = (-1) * number1;
+      }
+
+      if (suit2 == 'hearts' || suit2 == 'diamonds') {
+
+        number2 = (-1) * number2;
+      }
+    }
+
+    currentNumber1 = number1;
+
+    currentNumber2 = number2;
 
     $('#firstPlayerNumber').html(getCardDisplay(number1));
 
     $('#secondPlayerNumber').html(getCardDisplay(number2));
 
-    let suitCount1 = number1 < 11 ? number1 : 1;
+    let suitCount1 = Math.abs(number1) < 11 ? Math.abs(number1) : 1;
 
-    let suitCount2 = number2 < 11 ? number2 : 1;
+    let suitCount2 = Math.abs(number2) < 11 ? Math.abs(number2) : 1;
 
-    for (let i = 0; i < suitCount1; i++) {
+    for (let i = 0; i < Math.abs(suitCount1); i++) {
 
       $('#firstPlayerSuit').append(getSuitImage(suit1));
     }
 
-    for (let i = 0; i < suitCount2; i++) {
+    for (let i = 0; i < Math.abs(suitCount2); i++) {
 
       $('#secondPlayerSuit').append(getSuitImage(suit2));
     }
 
-    /////////////////////////////////////////////////////
-    // STORE PLAYED CARDS
-    /////////////////////////////////////////////////////
-
     playedCards.push(card1, card2);
 
-    /////////////////////////////////////////////////////
-    // CHECK WINNER
-    /////////////////////////////////////////////////////
+    if (compare === 1) {
 
-    if (number1 > number2) {
+      compareMath(number1, number2);
 
-      firstPlayer.push(...playedCards);
-
-      $('#winner').html("First Player Wins Round");
-
-      // RED BORDER
-
-      $('#firstPlayer').css('border-color', 'red');
-
-      playedCards = [];
+      $('#winner').html("Choose The Winning Card");
     }
 
-    else if (number2 > number1) {
+    else if (addition == 1 || subtraction == 1 || multiplication == 1) {
 
-      secondPlayer.push(...playedCards);
+      let symbol = "+";
 
-      $('#winner').html("Second Player Wins Round");
+      if (subtraction == 1) symbol = "-";
 
-      // RED BORDER
+      if (multiplication == 1) symbol = "×";
 
-      $('#secondPlayer').css('border-color', 'red');
-
-      playedCards = [];
+      $('#winner').html("Solve: " + currentNumber1 + " " + symbol + " " + currentNumber2);
     }
 
     else {
 
-      $('#winner').html("WAR!");
+      if (number1 > number2) {
 
-      handleWar();
-    }
+        player.push(...playedCards);
 
-    updateCounts();
+        $('#winner').html("Player Wins Round");
 
-    /////////////////////////////////////////////////////
-    // GAME OVER CHECK
-    /////////////////////////////////////////////////////
+        $('#firstPlayer').css('border-color', 'red');
 
-    if (firstPlayer.length === 0 || secondPlayer.length === 0) {
+        playedCards = [];
+      }
 
-      endGame();
+      else if (number2 > number1) {
+
+        computer.push(...playedCards);
+
+        $('#winner').html("Computer Wins Round");
+
+        $('#secondPlayer').css('border-color', 'red');
+
+        playedCards = [];
+      }
+
+      else {
+
+        $('#winner').html("WAR!");
+
+        handleWar();
+      }
+
+      updateCounts();
+
+      if (player.length === 0 || computer.length === 0) {
+
+        endGame();
+      }
     }
   }
 
@@ -250,164 +468,156 @@ $(document).ready(function () {
 
   function handleWar() {
 
-    /////////////////////////////////////////////////////
-    // CHECK ENOUGH CARDS
-    /////////////////////////////////////////////////////
+    if (player.length < 4) {
 
-    if (firstPlayer.length < 4) {
+      player = [];
 
-      firstPlayer = [];
-
-      $('#winner').html(
-        "Player 1 does not have enough cards. Player 2 Wins!"
-      );
+      $('#winner').html("Player does not have enough cards. Computer Wins!");
 
       endGame();
 
       return;
     }
 
-    if (secondPlayer.length < 4) {
+    if (computer.length < 4) {
 
-      secondPlayer = [];
+      computer = [];
 
-      $('#winner').html(
-        "Player 2 does not have enough cards. Player 1 Wins!"
-      );
+      $('#winner').html("Computer does not have enough cards. Player Wins!");
 
       endGame();
 
       return;
     }
-
-    /////////////////////////////////////////////////////
-    // HIDE SUITS
-    /////////////////////////////////////////////////////
-
-    $('#firstPlayerSuit').hide();
-
-    $('#secondPlayerSuit').hide();
-
-    /////////////////////////////////////////////////////
-    // CARD BACKS
-    /////////////////////////////////////////////////////
-
-    let backImg =
-      "<img style='height:14rem;' src='./images/cards.png'/>";
-
-    $('#firstPlayerNumber').html(backImg);
-
-    $('#secondPlayerNumber').html(backImg);
-
-    /////////////////////////////////////////////////////
-    // PLAY SOUNDS
-    /////////////////////////////////////////////////////
-
-    playSound(0);
-
-    playSound(1000);
-
-    playSound(1800);
-
-    /////////////////////////////////////////////////////
-    // FACE DOWN CARDS
-    /////////////////////////////////////////////////////
 
     for (let i = 0; i < 3; i++) {
 
-      playedCards.push(firstPlayer.shift());
+      playedCards.push(player.shift());
 
-      playedCards.push(secondPlayer.shift());
+      playedCards.push(computer.shift());
     }
 
     updateCounts();
 
-    /////////////////////////////////////////////////////
-    // DECIDING CARD
-    /////////////////////////////////////////////////////
-
-    setTimeout(function () {
-
-      $('#firstPlayerSuit').show();
-
-      $('#secondPlayerSuit').show();
-
-      let card1 = firstPlayer.shift();
-
-      let card2 = secondPlayer.shift();
-
-      let number1 = card1[0];
-
-      let suit1 = card1[1];
-
-      let number2 = card2[0];
-
-      let suit2 = card2[1];
-
-      $('#firstPlayerNumber').html(getCardDisplay(number1));
-
-      $('#secondPlayerNumber').html(getCardDisplay(number2));
-
-      $('#firstPlayerSuit').html(getSuitImage(suit1));
-
-      $('#secondPlayerSuit').html(getSuitImage(suit2));
-
-      playedCards.push(card1, card2);
-
-      /////////////////////////////////////////////////////
-      // WAR WINNER
-      /////////////////////////////////////////////////////
-
-      if (number1 > number2) {
-
-        $('#winner').html("First Player Wins WAR!");
-
-        $('#firstPlayer').css('border-color', 'red');
-
-        firstPlayer.push(...playedCards);
-
-        playedCards = [];
-      }
-
-      else if (number2 > number1) {
-
-        $('#winner').html("Second Player Wins WAR!");
-
-        $('#secondPlayer').css('border-color', 'red');
-
-        secondPlayer.push(...playedCards);
-
-        playedCards = [];
-      }
-
-      else {
-
-        $('#winner').html("WAR AGAIN!");
-
-        handleWar();
-      }
-
-      updateCounts();
-
-      /////////////////////////////////////////////////////
-      // FINAL GAME CHECK
-      /////////////////////////////////////////////////////
-
-      if (firstPlayer.length === 0 || secondPlayer.length === 0) {
-
-        endGame();
-      }
-
-    }, 2500);
+    drawCard();
   }
 
   /////////////////////////////////////////////////////
-  // BUTTON CLICK
+  // BUTTONS (UNCHANGED LOGIC EXCEPT DIRECTIONS FIXED)
   /////////////////////////////////////////////////////
 
   $('#draw').on('click', function () {
-
     drawCard();
+  });
+
+  $('#mathButton').on('click', function () {
+    $('#mathChoices').toggleClass('hidden');
+  });
+
+  $('#compareButton').on('click', function () {
+
+    compare = 1;
+
+    addition = 0;
+    subtraction = 0;
+    multiplication = 0;
+
+    hideDirections();
+
+    $('#integerChoices').removeClass('hidden');
+    $('#winnerButtons').removeClass('hidden');
+    $('#answerArea').addClass('hidden');
+
+    $('.compareDirections').removeClass('hidden');
+
+    $('#mathChoices').hide();
+  });
+
+  $('#arithmeticButton').on('click', function () {
+
+    compare = 0;
+
+    hideDirections();
+
+    $('#integerChoices').removeClass('hidden');
+    $('#arithmeticChoices').removeClass('hidden');
+    $('#answerArea').removeClass('hidden');
+    $('#winnerButtons').addClass('hidden');
+
+    $('#mathChoices').hide();
+  });
+
+  $('#addButton').on('click', function () {
+
+    addition = 1;
+    subtraction = 0;
+    multiplication = 0;
+
+    hideDirections();
+
+    $('.addDirections').removeClass('hidden');
+
+    $('#arithmeticChoices').hide();
+  });
+
+  $('#subtractButton').on('click', function () {
+
+    addition = 0;
+    subtraction = 1;
+    multiplication = 0;
+
+    hideDirections();
+
+    $('.subtractDirections').removeClass('hidden');
+
+    $('#arithmeticChoices').hide();
+  });
+
+  $('#multiplyButton').on('click', function () {
+
+    addition = 0;
+    subtraction = 0;
+    multiplication = 1;
+
+    hideDirections();
+
+    $('.multiplyDirections').removeClass('hidden');
+
+    $('#arithmeticChoices').hide();
+  });
+
+  $('#integerButton').on('click', function () {
+
+    integerChoice = 1;
+
+    hideDirections();
+
+    $('.integerDirections').removeClass('hidden');
+  });
+
+  $('#positiveButton').on('click', function () {
+
+    integerChoice = 0;
+
+    hideDirections();
+
+    $('.compareDirections').removeClass('hidden');
+  });
+
+  $('#player1WinnerButton').on('click', function () {
+
+    chosen = 1;
+    compareCheck();
+  });
+
+  $('#player2WinnerButton').on('click', function () {
+
+    chosen = 2;
+    compareCheck();
+  });
+
+  $('#submitAnswerButton').on('click', function () {
+    arithmeticCheck();
   });
 
 });
